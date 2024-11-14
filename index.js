d1368c8fb3af2c2aaae29f00b2cfa30a5b769b67
Goodluck-stores <goodlucksamuel2005@gmail.com>
2024-11-13 23:17:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// var name= "false"
+// console.log(name)
+// console.log(typeof(name))
+
+// let number = 5;
+// let secondnumber = 6;
+// let addition = number+secondnumber;
+// console.log(addition)
+// console.log(typeof(addition));
+
+// var value = 10;
+// var secondvalue = 2;
+// var subtract = value-secondvalue;
+// console.log(subtract)
+// console.log(typeof(subtract))
+
+// let input = 6;
+// let secondinput = 9;
+// let multiply = input*secondinput;
+// console.log(multiply);
+// console.log(typeof(multiply))
+
+// let num = 10;
+// let num2 = 10;
+// let divide = num > num2 && num < 9;
+// console.log(divide);
+// console.log(typeof(divide))
+
+
+// assignment
+
+let pi = 3.142;
+let radius = 20;
+let area = pi*radius*radius;
+console.log(area);
+console.log(typeof(area))
+
+var half = 1/2;
+var base = 10;
+var height = 15;
+var triangle = half*base*height;
+console.log(triangle)
+console.log(typeof(triangle))
+
+
+let principal = 10;
+let rate = 20;
+let time = 20.5;
+let divide = 100;
+let interest = principal*rate*time/divide;
+console.log(interest)
+console.log(typeof(interest));
+
+
+let a = 5;
+let b = 20;
+let c = 7;
+let solve =  (-b + Math.sqrt(b*b - 4*a*c)) / (2*a);
+let solution = (-b - Math.sqrt(b*b - 4*a*c)) / (2*a);
+console.log(solve)
+console.log(solution);
